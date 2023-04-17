421e5101ee04586b1b49a21fd78cf89e8c5a3b0c
Avi <avi.aminov@authomize.com>
2023-04-17 14:07:28 +0300

    - run on users simultaneously - save report - updated to readme

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
 # MFA Bombing Tester
 
 ## Introduction
-MFA (Multi-Factor Authentication) is a security measure used to protect user accounts by requiring more than one form of authentication. However, attackers can use a technique called MFA bombing to bypass MFA and gain access to user accounts.
+MFA (Multi-Factor Authentication) is a security measure
+used to protect user accounts by requiring more than one
+form of authentication. In this way, if one factor is
+compromised (e.g. a password), the account can still be protected.
+However, attackers can use a technique called MFA bombing to
+bypass MFA and gain access to user accounts.
 
-MFA bombing involves sending a large number of authentication requests to a user's account, overwhelming the user and tricking them into approving a fraudulent request. This technique can be used to bypass MFA that uses push notifications.
+MFA bombing involves sending a large number of authentication
+requests to a user's account, overwhelming the user and
+tricking them into approving a fraudulent request. This
+technique can be used to bypass MFA that uses push notifications.
 
-To protect your users from MFA bombing, it's important to educate them on the topic and encourage them to be cautious when approving authentication requests.
+To protect your users from MFA bombing, it's important to educate
+them on the topic and encourage them to be cautious when
+approving authentication requests.
 
 ## About the Tool
-The MFA Bombing Tester is a tool that scans your Okta organization for users with push authentication factor. When it finds one, it sends a push notification to the user to make them click on it. This helps you test your organization's resilience to MFA bombing attacks and identify potential vulnerabilities.
+The MFA Bombing Tester is a tool that scans your Okta organization
+for users with push authentication factor. When it finds one,
+it sends a push notification to the user to make them click on it.
+This helps you test your organization's resilience to MFA bombing
+attacks and identify potential vulnerabilities.
 
-The tool is written in Python and uses the Okta API to scan your organization for push-enabled users. It then sends a push notification to each user and waits for a response. If a user clicks on the push notification, the tool reports a successful authentication attempt.
+The tool is written in Python and uses the Okta API to scan
+your organization for push-enabled users. It then sends a push
+notification to each user and waits for a response.
+If a user clicks on the push notification, the tool reports
+a successful authentication attempt. In the end a report is saved
+to a CSV file containing the list of users along with the results
+of the test for each one.
 
 
 ## How to Use the Tool
@@ -26,16 +46,17 @@ The tool is written in Python and uses the Okta API to scan your organization fo
 2. Create the file `config.yaml` with your Okta config (use `config.yaml.example` for reference)
 3. Run using Poetry: 
 ```commandline
-poetry run python mfa_bombing_tester.py
+poetry run python mfa_bombing_tester.py [path/to/report.csv]
 ```
+4. Read output report (default is to save to `report.csv`). 
+It contains the columns user_id, user_email, and result.
 
 If you don't want to use Poetry, you can use python but need to install the Okta SDK first
 ```commandline
-pip install PyYAML okta
-python mfa_bombing_tester.py
+pip install fire PyYAML okta
+python mfa_bombing_tester.py [path/to/report.csv]
 ```
 
 
 ## Future work
-* Add concurrency
 * Support more platforms (AzureAD, Google Workspace, PingIdentity)
--- mfa_bombing_tester.py
+++ mfa_bombing_tester.py
@@ -1,9 +1,11 @@
 import asyncio
 from asyncio import sleep
 from pathlib import Path
+from time import time
 from typing import List
 
 import yaml
+from fire import Fire
 from okta import models
 from okta.client import Client as OktaClient
 
@@ -17,42 +19,63 @@ class MFAChallenger:
             'orgUrl': config['okta_domain'],
             'token': config['okta_token'],
         }
-        print(f"Running MFA bombing tests on org {config['okta_domain']}")
+        print(f"Running MFA bombing tests on org {config['okta_domain']}."
+              f" Searching for users with push notification factor configured.")
         self.okta_client = OktaClient(okta_config)
 
     async def challenge_user(self, user: models.User):
         factors, resp, err = await self.okta_client.list_factors(user.id)
         factors: List[models.PushUserFactor]  # type hinting
         push_factors = [factor for factor in factors if factor.factor_type == 'push']
+        if len(push_factors) == 0:
+            return "No push factors found"
+        if len(push_factors) > 1:
+            print(f"Warning: User {user.profile.email} has more than one push factor. "
+                  f"The program will only challenge one of them.")
         for factor in push_factors:
             print(f"Found push factor for user {user.profile.email}. Challenging.")
-            verify_factor_transaction, verify_factor_response, err = await self.okta_client.verify_factor(
+            verify_factor_transaction, _, err = await self.okta_client.verify_factor(
                 user.id, factor.id, models.VerifyFactorRequest(),
             )
             transaction_id = verify_factor_transaction.links['poll']['href'].split("/")[-1]
+            start_time = time()
             while True:
                 polling_factor_response, _, err = await self.okta_client.get_factor_transaction_status(
                     user.id,
                     factor.id,
                     transaction_id,
                 )
+                time_passed = time()-start_time
+                if round(time_passed) % 20 == 0:
+                    print(f"Still waiting for response or timeout from user {user.profile.email}"
+                          f" (waited {round(time_passed)} seconds)")
                 polling_factor_response: models.VerifyUserFactorResponse
                 if polling_factor_response.factor_result != 'WAITING':
                     if polling_factor_response.factor_result == 'SUCCESS':
                         print(f"ALERT: User {user.profile.email} with id {user.id} approved the push notification")
+                        return "User approved the challenge"
                     else:
-                        print(f"User {user.profile.email} didn't approve the push notification")
-                    break
+                        # print(f"User {user.profile.email} didn't approve the push notification")
+                        return "User rejected the challenge"
 
                 await sleep(1)
 
     async def search_and_challenge_users(self):
         users, resp, err = await self.okta_client.list_users()
-        for user in users:
-            # print(user.profile.first_name, user.profile.last_name)
-            await self.challenge_user(user)
+        tasks = [asyncio.create_task(self.challenge_user(user)) for user in users]
+        results = await asyncio.gather(*tasks)
+        return users, results
+
+
+def mfa_bombing_tester(output_file_path="report.csv"):
+    mfa_challenger = MFAChallenger()
+    users, results = asyncio.run(mfa_challenger.search_and_challenge_users())
+    with Path(output_file_path).open('w') as f:
+        f.write("user_id,user_email,result\n")
+        for user, result in zip(users, results):
+            f.write(",".join([user.id, user.profile.email, result]))
+            f.write("\n")
 
 
 if __name__ == '__main__':
-    mfa_challenger = MFAChallenger()
-    asyncio.run(mfa_challenger.search_and_challenge_users())
+    Fire(mfa_bombing_tester)
